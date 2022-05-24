21c81784a16a5b125fa57a990f5f4e1432cf6cac
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-23 23:16:15 -0300

    Initial commit

---
+++ src/main/java/com/example/ranni/RanniApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ranni/RanniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ranni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
