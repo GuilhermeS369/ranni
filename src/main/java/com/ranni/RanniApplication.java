d846c6b08be8c092ee1ce4de4fd6bd89c6728276
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-24 22:11:35 -0300

    H2 database, test profile, JPA PT 2

--- src/main/java/com/ranni/RanniApplication.java
+++ src/main/java/com/ranni/RanniApplication.java
@@ -8,7 +8,8 @@ public class RanniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RanniApplication.class, args);
-
+		
+		
 	}
 
 }
--- src/main/java/com/ranni/entities/User.java
+++ src/main/java/com/ranni/entities/User.java
@@ -3,25 +3,37 @@ package com.ranni.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {	
-		
-	}
 	
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
+		
+		
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+	public User() {	
+		
 	}
 
 	public Long getId() {
