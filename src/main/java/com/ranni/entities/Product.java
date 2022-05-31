ccd1c88017ef7dc734c5a9fe079fbec3fa387c7c
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-30 22:05:18 -0300

    Product-OrderItem one-tomany association

--- src/main/java/com/ranni/entities/Product.java
+++ src/main/java/com/ranni/entities/Product.java
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -26,18 +29,19 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@ManyToMany	
-	
-				// NOME DA TABELA             
-	@JoinTable(name = "tb_product_category", 
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
-	
-	private Set <Category> categories = new HashSet<>();
-	
+
+	@ManyToMany
+
+	// NOME DA TABELA
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Product() {
-		
+
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -89,9 +93,16 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-	
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
