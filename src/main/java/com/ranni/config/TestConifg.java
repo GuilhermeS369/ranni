fca69e69c38847f31100b0e50fcdc1503a4ad3ab
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-31 23:16:26 -0300

    Payment, one-to-one association

--- src/main/java/com/ranni/config/TestConifg.java
+++ src/main/java/com/ranni/config/TestConifg.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ranni.entities.Category;
 import com.ranni.entities.Order;
 import com.ranni.entities.OrderItem;
+import com.ranni.entities.Payment;
 import com.ranni.entities.Product;
 import com.ranni.entities.User;
 import com.ranni.entities.enums.OrderStatus;
@@ -83,9 +84,10 @@ public class TestConifg implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);		
+		o1.setPayment(pay1);
 		
-		
-		
+		orderRepository.save(o1);
 		
 		
 		
