9e2ec18eb5f56a96e53fb62fa108f0edea04e3da
Guilherme Santos <guilherme460@yahoo.com.br>
2022-06-01 22:24:48 -0300

    User Insert

--- src/main/java/com/ranni/resources/UserResource.java
+++ src/main/java/com/ranni/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.ranni.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ranni.entities.User;
 import com.ranni.services.UserService;
@@ -33,14 +37,47 @@ public class UserResource {
 
 	// ----------------------------------------------------------------------------------------
 	// ENDPOINT DO CONTROLADORREST
+	// CAMINHO PARA ACESSAR:
 	@GetMapping(value = "/{id}")
-	
-	public ResponseEntity <User> findById(@PathVariable Long id) {
-	
+
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+
 		User obj = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(obj);
+
+	}
+
+	// ----------------------------------------------------------------------------------------
+	// OPERAÇÃO PARA INSERIR O USUARIO
+	// METODO QUE RECEBE O POST DA URL E ENVIA PRO BD
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		// VARIAVEL DO TIPO URI
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
