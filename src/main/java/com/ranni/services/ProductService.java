f5855f65e1fb39c8c1297c50e3ca000354371646
Guilherme Santos <guilherme460@yahoo.com.br>
2022-06-08 15:06:03 -0300

    Insert, delete, update Product

--- src/main/java/com/ranni/services/ProductService.java
+++ src/main/java/com/ranni/services/ProductService.java
@@ -3,27 +3,73 @@ package com.ranni.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ranni.entities.Product;
 import com.ranni.respositories.ProductRepository;
+import com.ranni.services.exceptions.DatabaseException;
+import com.ranni.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
 	@Autowired
 	private ProductRepository repository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
-		//TIPO OPTIONAL ARMAZENA O OBJ
+		// TIPO OPTIONAL ARMAZENA O OBJ
 		Optional<Product> obj = repository.findById(id);
-		//RETORNA O OBJ
+		// RETORNA O OBJ
 		return obj.get();
 	}
+
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+
+		}
+	}
+
+	public Product update(Long id, Product obj) {
+
+		try {
+			// PREPARA PARA RECEBER UM TIPO
+			Product entity = repository.getReferenceById(id);
+			// ATUALIZA A ENTITY COM OS DADOS DO OBJ
+			updateData(entity, obj);
+			// SALVA A ENTITY
+			return repository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
 	
-	
+	//METODO ONDE VC SELECIONA OQ QUER DAR UPDATE
+		private void updateData(Product entity, Product obj) {
+			entity.setName(obj.getName());
+			entity.setDescription(obj.getDescription());
+			entity.setImgUrl(obj.getImgUrl());
+			entity.setPrice(obj.getPrice());
+		
+		}
+
 }
