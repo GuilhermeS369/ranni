5af341c34bbeb451e33dbfd1b033c6f37e80ac86
Guilherme Santos <guilherme460@yahoo.com.br>
2022-08-05 20:32:06 -0300

    Swagger v2

--- src/main/java/com/ranni/RanniApplication.java
+++ src/main/java/com/ranni/RanniApplication.java
@@ -3,7 +3,10 @@ package com.ranni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(scanBasePackages = {"com.zuoyan.springboot.appmissionhall"})
+@EnableSwagger2
 public class RanniApplication {
 
 	public static void main(String[] args) {
